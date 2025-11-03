710912c8b1ca5813c7887f94b7cd27be9e449785
Ankit <ankitsingh110198@gmail.com>
2025-11-03 15:47:42 +0530

    frontend

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://amarneerfuelstationbackend.vercel.app"
+      ? "https://amarneerfuelstationbackend.onrender.com/"
       : "http://localhost:5000", // your local backend during dev
 });
 
